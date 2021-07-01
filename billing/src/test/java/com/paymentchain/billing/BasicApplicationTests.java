3756efb5c4bf3cb0d9e2081a90ad7a73bdad1122
unknown <you@example.com>
2021-07-01 12:43:17 -0500

    Comentar las partes test 02.

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 //@SpringBootTest
 public class BasicApplicationTests {
 
+//Aca hice un cambio
 	@Test
 	public void contextLoads() {
 		
